dd13996ac4329a26593e9d49c64b932be1f7562c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-06 21:21:57 -0300

    added question form to modal

--- surveys/forms.py
+++ surveys/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from django.core.mail import send_mail
+from django.forms import inlineformset_factory
 
 from surveys.models import Question
 
 
-class QuestionForm(forms.Form):
+class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['question_text']
+        fields = ['question_text', ]
